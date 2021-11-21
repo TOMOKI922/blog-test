a97a691336e6401f4016bf579187359a0c1930b0
muratasyuzo <kohu241@gmail.com>
2021-11-21 12:45:48 +0900

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,47 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Layout from "../components/layout"
+import * as style from "../styles/index.module.scss"
+const Index = () => {
+  return (
+    <Layout>
+      <div className={style.hero}>
+        <Image src="/images/index-hero.jpg" alt="hero" layout="fill" objectFit="cover" quality={90} />
+        <div className={style.textContainer}>
+          <h1>I'm Abe  Hiroki!</h1>
+          <h3>JavaScript</h3>
+        </div>
+      </div>
+      <div className={style.container}>
+        <div className={style.profile}>
+          <div>
+            <h2>JavaScriptNerd</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </div>
+          <Image src="/images/profile.jpg" alt="hero" height={1195} width={1000} quality={90} />
+        </div>
+        <div className={style.skillsContainer}>
+          <h2>Skills</h2>
+          <div>
+            <div>
+              <img src="/images/javascript.svg" alt="javascript" /><span>JavaScript</span>
+            </div>
+            <div>
+             <img src="/images/react.svg" alt="react" /><span>React / 5 years</span>
+            </div>
+            <div>
+              <img src="/images/gatsby.svg" alt="gatsby"/><span>Gatsby / 3 years</span>
+            </div>
+            <div>
+              <img src="/images/next.svg" alt="next" /><span>Next.JS / 3 years</span>
+            </div>
+          </div>
+        </div>
+        <div className={style.ctaButton}>
+          <Link href="/contact"><a>Make It Happen!</a></Link>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+export default Index
